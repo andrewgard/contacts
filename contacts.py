9513636f984b46002c7106ddca09c81927d9ee01
Christopher <christopher@Christopher_Tab.(none)>
2014-05-03 15:39:52 -0400

    added basic structure

---
+++ contacts.py
@@ -0,0 +1,23 @@
+
+class contact:
+
+	def __init__(self, data):
+
+	def __str__(self):
+
+class group:
+
+	def __init__(self, name):
+		self.name = name
+		self.contacts = []
+
+	def __str__(self):
+
+class book:
+
+	def __init__(self, data, name):
+		self.groups = []
+		self.name = name
+		self.contacts = []
+
+	def __str__(self):
