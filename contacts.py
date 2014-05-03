a4b72e9a8615e6e1f9d190af8d5b18ada45690ee
Andrew <andrew@andrew-VirtualBox.(none)>
2014-05-03 17:05:10 -0400

    started adding functionality for Groups

--- contacts.py
+++ contacts.py
@@ -1,19 +1,36 @@
 
-class contact:
+class Contact:
 
 	def __init__(self, data):
 
 	def __str__(self):
-
-class group:
-
+				
+class Group:
 	def __init__(self, name):
-		self.name = name
-		self.contacts = []
+		self.name = name 
+		self.members = [] 	
+		
 
 	def __str__(self):
+		return self.name + "\nmembers: " + ", ".join(self.members)		
 
-class book:
+	def add(self, contact):
+		self.members.append(contact)
+
+	def remove(self, firstname, lastname):
+		for contact in self.members:
+			if (contact.first == firstname && contact.last == lastname):
+				self.members.remove(contact)
+	
+	def search(self, searchparam, val):
+		if(searchparam == 'first'):
+			for contact in self.members:
+				if(contact.first == val):
+					return contact
+
+		 	
+		
+class Book:
 
 	def __init__(self, data, name):
 		self.groups = []
