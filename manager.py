a8053e761f65276f8c373d59f81e92fc573601f6
Christopher <christopher@Christopher_Tab.(none)>
2014-05-06 21:38:41 -0400

    added help function to manager

--- manager.py
+++ manager.py
@@ -3,11 +3,17 @@
 import helper as h
 import sys
 
+def print_help():
+	print("Commands:")
+	print("\tadd contact <last name> <first name>\t\t\t\t-Add a new contact to the current contact book.")
+	print("\tadd group <group name>\t\t\t\t\t\t-Add a new group to the current contact book.")
+	print("\tprint [contact <last name> <first name> | group <name> | all]\t-Prints corresponding value.")
+	print("\tsearch <first name> <last name> [group <name> | all]\t\t-Search a group or all contacts.")
+
 if(len(sys.argv) < 2):
 	print("Need a contact book .yaml file")
 	sys.exit()
 
 data = h.load(sys.argv[1])
-
 thebook = h.Book(data, 'Book 1')
-print(thebook)
+print_help()
